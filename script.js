8d82fa8ff9f76785854d6a023c718f2d5b6e2b92
Gr8ness <pmleechapman@gmail.com>
2022-05-06 10:48:04 -0400

    wip: removed css tag in html. Able to display movie poster

--- script.js
+++ script.js
@@ -5,16 +5,31 @@
 // 2. The Browser's fetch() function 
 // 3. Using the Axios hhtp client library
 
+// VARIABLES
 const URL = "http://www.omdbapi.com/?apikey=a71b148a&t=Die+Hard";
+
+// ELEMENT REFERENCES jQuery variables
+const $title = $('#title');
+const $year = $('#year');
+const $rating = $('#rated');
+
+// EVENT LISTENERS
+
+
+// FUNCTIONS
 
 function handleGetData(){
 
     $.ajax(URL).then(function(data) {
         console.log('movie data is ready')
-        console.log(data)
+        // console.log(data)
+        $title.text(data.Title)
+        $year.text(data.Year)
+        $rating.text(data.Rated)
+        $('main').append(`<img src="${data.Poster}"/>`)
     }, function(error) {
         console.log('something is wrong')
         console.log(error)
     })
-    
+
 }
